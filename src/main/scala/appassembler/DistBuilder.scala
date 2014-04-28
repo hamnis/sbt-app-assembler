c2a9d2f358cfe54dea334df5110dac1505d75466
Erlend Hamnaberg <erlend@hamnaberg.net>
2014-04-28 15:11:58 +0200

    Do not crash if the target file does not exist

--- src/main/scala/appassembler/DistBuilder.scala
+++ src/main/scala/appassembler/DistBuilder.scala
@@ -11,7 +11,9 @@ object DistBuilder {
   val permissions = Map("/bin/*" -> FilePermissions(Integer.decode("0755")).get)
 
   def create(conf: AppConfig, bin: Jar, classpath: Def.Classpath)(implicit logger: Logger): File = {
-    archiver.IO.delete(conf.output) //TODO: Should this really be required?
+    if (conf.output.exists) {
+      archiver.IO.delete(conf.output) //TODO: Should this really be required?
+    }
 
     IO.withTemporaryDirectory{ temp => 
       val distBinPath = temp / "bin"
